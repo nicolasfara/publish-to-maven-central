59018ad717b782a4fe232fc8c80ff8007cbc6070
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-12-25 09:25:17 +0100

    docs: Add javadoc on extension

--- src/main/kotlin/it/nicolasfarabegoli/gradle/central/PublishToMavenCentralExtension.kt
+++ src/main/kotlin/it/nicolasfarabegoli/gradle/central/PublishToMavenCentralExtension.kt
@@ -3,13 +3,43 @@ package it.nicolasfarabegoli.gradle.central
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
 
+/**
+ * Extension used to configure the plugin for the [project].
+ */
 open class PublishToMavenCentralExtension(private val project: Project) {
+    /**
+     * Project name.
+     */
     val projectName: Property<String> = project.propertyWithDefault(project.name)
+
+    /**
+     * Project description.
+     */
     val projectDescription: Property<String> = project.propertyWithDefault(project.description)
+
+    /**
+     * Name of the license. Use the SPDX short identifier: https://opensource.org/licenses.
+     */
     val licenseName: Property<String> = project.propertyWithDefault("Apache-2.0")
+
+    /**
+     * The URL of the chose license.
+     */
     val licenseUrl: Property<String> = project.propertyWithDefault("https://www.apache.org/licenses/LICENSE-2.0")
+
+    /**
+     * SCM connection URL.
+     */
     val scmConnection: Property<String> = project.propertyWithDefault("git:git@github.com:nicolasfara/${project.name}")
+
+    /**
+     * Project's URL, typically a GitHub URL repo.
+     */
     val projectUrl: Property<String> = project.propertyWithDefault("https://github.com/nicolasfara/${project.name}")
+
+    /**
+     * Configuration for the Maven Central repository.
+     */
     val mavenCentral: Repository = Repository(
         Repository.mavenCentralName,
         url = Repository.mavenCentralUrl,
